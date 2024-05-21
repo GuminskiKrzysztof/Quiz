11cce112fd9dd7a46b9ef5b3ba7b0de307b28842
GuminskiKrzysztof <krzysztof.guminski629@gmail.com>
2024-05-21 11:09:40 +0200

    connecting to database

--- QUIZ/Model/Question.cs
+++ QUIZ/Model/Question.cs
@@ -9,17 +9,20 @@ namespace QUIZ.Model
     public class Question
     {
         private string text;
+        private int which;
         public List<Answer> answers; 
         
         public Question()
         {
             this.text = string.Empty;   
+            this.which = 0;
             this.answers = new List<Answer>();
         }
 
-        public Question(string text, List<Answer> answers)
+        public Question(string text, int which, List<Answer> answers)
         {
             this.text = string.Empty;
+            this.which = which;
             this.answers = answers;
         }
 
